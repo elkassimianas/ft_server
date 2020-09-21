3a7e4a325158c90d0d9281db99828dd114361312
EL kassimi Anas <57335519+ael-kass@users.noreply.github.com>
2020-09-21 10:59:35 +0100

    Update script.sh

--- srcs/script.sh
+++ srcs/script.sh
@@ -4,6 +4,6 @@ service php7.3-fpm start
 chown -R  mysql: /var/lib/mysql
 service mysql start
 echo "GRANT ALL PRIVILEGES ON *.* TO 'ael-kass'@'localhost' IDENTIFIED BY 'anas123';" | mysql -u root
-echo "CREATE DATABASE hamid;" | mysql -u root
-mysql -u root hamid < /root/hamid.sql
+echo "CREATE DATABASE anas;" | mysql -u root
+mysql -u root anas < /root/wordpress.sql
 service nginx start
